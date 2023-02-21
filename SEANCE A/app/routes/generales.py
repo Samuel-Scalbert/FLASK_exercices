81d19de7cf7b14389030bc1068a7b14fe6f55e88
Scalbert Samuel <sam.scalbert@gmail.com>
2023-02-21 15:58:05 +0100

    "sarah"

---
+++ SEANCE A/app/routes/generales.py
@@ -0,0 +1,15 @@
+from ..app import app
+from flask import render_template
+from ..app import app,db
+from sqlalchemy import text
+from ..models.factbook import Country
+import json
+
+@app.route("/")
+def accueil():
+    return render_template("pages/accueil.html")
+
+@app.route("/pays")
+def pays():
+    pays = Country.query.all()
+    return render_template("pages/pays.html", data = pays)
