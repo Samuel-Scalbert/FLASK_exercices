1f1960760014c5f588800988797ccfa4d541eea8
Scalbert Samuel <sam.scalbert@gmail.com>
2023-03-16 16:41:21 +0100

    ajout exercice rev 2 seance 3

---
+++ SEANCE B/app/models/users.py
@@ -0,0 +1,38 @@
+from ..app import app, db
+from werkzeug.security import generate_password_hash
+
+class Users(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True, autoincrement=True)
+    prenom = db.Column(db.Text, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    from werkzeug.security import generate_password_hash
+    @staticmethod
+    def ajout(prenom, password):
+    erreurs = []
+    if not prenom:
+        erreurs.append("Le prénom est vide")
+    if not password or len(password) < 6:
+        erreurs.append("Le mot de passe est vide ou trop court")
+
+    unique = Users.query.filter(
+        db.or_(Users.prenom == prenom)
+    ).count()
+    if unique > 0:
+        erreurs.append("Le prénom existe déjà")
+
+    if len(erreurs) > 0:
+        return False, erreurs
+
+    utilisateur = Users(
+        prenom=prenom,
+        password=generate_password_hash(password)
+    )
+
+    try:
+        db.session.add(utilisateur)
+        db.session.commit()
+        return True, utilisateur
+    except Exception as erreur:
+        return False, [str(erreur)]
